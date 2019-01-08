d839f9e72f983e8ac2dfd995a5a250aa43c699ae
Alwin <824826557@qq.com>
2019-01-08 11:23:48 +0800

    第二次上传

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'antd-mobile/dist/antd-mobile.css'
+// import router from "./router";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
