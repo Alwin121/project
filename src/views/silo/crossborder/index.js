6ae50fdad225621aacb240388abebcf921f18e9e
Alwin <824826557@qq.com>
2019-01-08 15:08:58 +0800

    第二次上传

--- src/views/silo/crossborder/index.js
+++ src/views/silo/crossborder/index.js
@@ -1,12 +1,53 @@
 import React,{Component} from "react"
+import { PullToRefresh, Button } from 'antd-mobile';
 
-class Center extends Component {
-	render(){
-		console.log(this);
-		return <div>
-			Center
-		</div>
+class Demo extends React.Component {
+	constructor(props) {
+	  super(props);
+	  this.state = {
+		refreshing: false,
+		down: true,
+		height: document.documentElement.clientHeight,
+		data: [],
+	  };
 	}
-}
+  
+	componentDidMount() {
+	  const hei = this.state.height - ReactDOM.findDOMNode(this.ptr).offsetTop;
+	  setTimeout(() => this.setState({
+		height: hei,
+		data: genData(),
+	  }), 0);
+	}
+  
+	render() {
+	  return (<div>
+		<PullToRefresh
+		  damping={60}
+		  ref={el => this.ptr = el}
+		  style={{
+			height: this.state.height,
+			overflow: 'auto',
+		  }}
+		  indicator={this.state.down ? {} : { deactivate: '上拉可以刷新' }}
+		  direction={'up'}
+		  refreshing={this.state.refreshing}
+		  onRefresh={() => {
+			this.setState({ refreshing: true });
+			setTimeout(() => {
+			  this.setState({ refreshing: false });
+			}, 1000);
+		  }}
+		>
+		  {this.state.data.map(i => (
+			<div key={i} style={{ textAlign: 'center', padding: 20 }}>
+			  {this.state.down ? 'pull down' : 'pull up'} {i}
+			</div>
+		  ))}
+		</PullToRefresh>
+	  </div>);
+	}
+  }
 
-export default Center
+
+export default Demo
