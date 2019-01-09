9310b98e60d99358591d184d79fcc62222474d6f
xushaung <xushuang@163.com>
2019-01-09 10:56:04 +0800

    第一次提交

--- src/views/silo/index.js
+++ src/views/silo/index.js
@@ -4,10 +4,7 @@ import React,{Component} from "react"
 class Silo extends Component {
 	render(){
 		return <div>
-<<<<<<< HEAD
 			
-=======
->>>>>>> fdd0f60060acaafb42febed8bc5620041e25d5cc
 			{this.props.children}
 		</div>
 	}
