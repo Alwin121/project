7ed32ffafa6a328487a9dbe1aca6e5d983f8345c
Alwin <824826557@qq.com>
2019-01-10 10:14:19 +0800

    上传路由

--- src/views/prodectdetail/index.js
+++ src/views/prodectdetail/index.js
@@ -1,9 +1,18 @@
 import React,{Component} from "react"
-
+import Top from '../../components/top'
+import axios from "axios";
 class Prodectdetail extends Component {
+	componentDidMount(){
+		axios({
+			url:"http://www.mei.com/appapi/product/detail/v3?categoryId=2041204190000005269&productId=2042204299000798741&userId=2022202299900101332&platform_code=H5&timestamp=1547085993959&summary=8cf0839f241fbeaba8467685ee0ae589"
+		}).then(res=>
+			console.log(res.data.infos)
+			)
+	}
 	render(){
-		console.log(this);
+		
 		return <div>
+			<Top {...this.props}></Top>
 			Prodectdetail
 		</div>
 	}
