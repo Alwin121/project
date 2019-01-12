b967a9f54e7a8c2ef0d91fe57c631b1cc25586b2
xushaung <xushuang@163.com>
2019-01-12 10:15:29 +0800

    合

--- src/views/shoppingcar/index.js
+++ src/views/shoppingcar/index.js
@@ -10,7 +10,8 @@ class Shoppingcar extends Component {
 		
 		this.state = {
 			datalist:[],
-			allChecked:false,
+			
+			checkedAll:false,
 			shoppingcarlist:[
 				{	
 					id:1,
@@ -19,7 +20,7 @@ class Shoppingcar extends Component {
 					number:1,
 					url:"https://cdn15.mei.com/product/GVC-245-00011/GVC-245-00011a.jpg@182w_242h_2e_65q",
 					description:"黑马纹棒球领皮外套",
-					isCheck:false
+					checked:false
 				},
 				{	
 					id:2,
@@ -28,13 +29,14 @@ class Shoppingcar extends Component {
 					number:1,
 					url:"https://cdn15.mei.com/product/PL5-409-00189/PL5-409-00189a.jpg@225w_300h_2e_100q",
 					description:"灰黑色拼接女士长靴",
-					isCheck:false
+					checked:false
 				}
 			]
 		}
 	}
 
 	componentDidMount(){
+	
 		axios({
 			url:"http://www.mei.com/appapi/product/maybeLike/v3?pageIndex=1"
 		}).then(res=>{
@@ -54,13 +56,18 @@ class Shoppingcar extends Component {
 					<ul className="products_ul">
 						{
 							this.state.shoppingcarlist.map((list,index)=>{
+								{console.log(list)}
 								return (
 									<li key={list.name}>
-										<label onClick={this.handleClick.bind(this)}><input type="checkbox" /></label>
+									
+										<label >
+										<input type="checkbox" checked={list.checked} 
+										onChange={(e)=>this.handleClick(index,e)}/></label>
 										<img src={list.url}/>
 										<div className="products_right">
 											<p>{list.name}</p>
 											<p>
+												
 												<button onClick={this.handleAddClick.bind(this,index)}>+</button>
 												<span>{list.number}</span>
 												<button onClick={this.handleSubClick.bind(this,index)}>-</button>
@@ -74,8 +81,8 @@ class Shoppingcar extends Component {
 							})
 						}
 						<div className="products_all">
-							<label><input type="checkbox"  onClick={this.onAllClick.bind(this)} />全选</label>
-							<p>总金额:</p>
+							<label><input type="checkbox" ref="quanxuan" onChange={(e)=>{this.onAllClick(e)}}/>全选</label>
+							{/* <p>总金额:</p> */}
 						</div>
 					</ul>
 				:<div className="shoppingcar_div_box">
@@ -107,27 +114,62 @@ class Shoppingcar extends Component {
 		this.setState({
 			shoppingcarlist:list
 		})
-
+		
 		
 	}
-	handleSubClick(index){
+
+	handleSubClick(index,number){
+		
 		var list2 = [...this.state.shoppingcarlist]
 		list2[index].number--
+		if(list2[index].number<0){
+			list2[index].number=0
+		}
 		this.setState({
 			shoppingcarlist:list2
 		}) 
 	}
 
-	onAllClick(){
-		
-		this.setState({
-			allChecked:!this.state.allChecked
-		})
-	}
+	onAllClick(e){
+		console.log(e);
+		if(e.target.checked==true){
+            this.setState({
+                shoppingcarlist:this.state.shoppingcarlist.map((ele,index)=>{
+					console.log(ele)
+                    ele.checked=true
+                    return ele
+                })
+            })
+ 
+        }else if(e.target.checked==false){
+			console.log(e.target.checked);
+            this.setState({
+			
+                shoppingcarlist:this.state.shoppingcarlist.map((ele,index)=>{
+					ele.checked=false
+                    return ele
+                })
+            })
+        }
 
-	handleClick(index){
-		
+	}
 	
+	handleClick(i,e){
+		var ccc=this.state.shoppingcarlist.map((ele,index)=>{
+				
+			if(index==i){
+				console.log(ele.checked)
+				ele.checked=e.target.checked
+				return ele
+			}else {
+				return ele
+			}
+		})
+				this.setState({
+					shoppingcarlist:ccc
+				})
+			
+		
 	}
 
 
@@ -140,6 +182,23 @@ class Shoppingcar extends Component {
 			shoppingcarlist:mylist
 		})
 	}
+	componentDidUpdate(){
+       
+        var bool=this.state.shoppingcarlist.every((ele,index)=>{
+			console.log(ele)
+            if(ele.checked==true){
+                return true
+            }else {
+                return false
+            }
+        })
+		
+        if(bool==true){
+            this.refs.quanxuan.checked=true
+        }else {
+            this.refs.quanxuan.checked=false
+        }
+    }
 
 	
 }
